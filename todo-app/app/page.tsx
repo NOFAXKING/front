55b00f254ba39edb35d3025ea0072240f7d92e33
Havre Loic <dongmoabenahavreloic030@gmail.com>
2024-11-04 17:14:31 +0100

    Front structure completed

--- todo-app/app/page.tsx
+++ todo-app/app/page.tsx
@@ -2,7 +2,26 @@
 export default function Home() {
   return (
     <>
-      <h1>ToDo App</h1>
+      <h1 className="flex justify-center font-medium text-3xl mb-10">Well come back UserName</h1>
+      <div className="flex justify-center min-h-screen py-2">
+        <div>
+          <div className="mb-10">
+            <label htmlFor="todo-note" className="font-medium text-2xl">Add a Task</label>
+            <input type="text" id="todo-note" className="border-2 border-black" />
+          </div>
+          <div>
+            <h1 className="font-extrabold">List of Tasks</h1>
+            <ul>
+              <li>
+                First task
+              </li>
+              <li>
+                Second task
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
