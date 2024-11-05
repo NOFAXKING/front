2f8e554cd5f59b3d1b0fb45be2f926e05a6679d5
Havre Loic <dongmoabenahavreloic030@gmail.com>
2024-11-05 11:22:43 +0100

    Add button to form

--- todo-app/app/page.tsx
+++ todo-app/app/page.tsx
@@ -5,10 +5,11 @@ export default function Home() {
       <h1 className="flex justify-center font-medium text-3xl mb-10">Well come back UserName</h1>
       <div className="flex justify-center min-h-screen py-2">
         <div>
-          <div className="mb-10">
+          <form className="mb-10">
             <label htmlFor="todo-note" className="font-medium text-2xl">Add a Task</label>
-            <input type="text" id="todo-note" className="border-2 border-black" />
-          </div>
+            <input type="text" id="todo-note" className="border-2 border-black p-1" />
+            <input type="submit" value="Add" className="border-2 border-black p-1" />
+          </form>
           <div>
             <h1 className="font-extrabold">List of Tasks</h1>
             <ul>
